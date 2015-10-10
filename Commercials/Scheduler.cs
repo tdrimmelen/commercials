42cc9a133e72d5b6f1cf8937fea6cc7aa548f0f4
tdrimmelen <theo@vandrimmelen.eu>
2015-10-11 00:06:32 +0200

    First version

---
+++ Commercials/Scheduler.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace Commercials
+{
+
+    class Scheduler
+    {
+        public enum EventType { Show, Hide};
+
+        private class CommercialEvent
+        {
+            public EventType type { get; set; }
+            public TimeSpan offset { get; set; }
+            public string commercial { get; set; }
+
+        }
+
+        Queue<CommercialEvent> theQueue;
+        Queue<CommercialEvent>.Enumerator theEnum;
+
+        private Action<string> theDelegate;
+        private System.Timers.Timer theTimer;
+        private Dispatcher theDispatcher;
+        private DateTimeOffset theLastTime; // Time that last event occured
+        private TimeSpan theLastRelTime; // Relative time last event occured
+
+        private System.Object theLock;
+ 
+
+        public Scheduler(Dispatcher aDispatcher, Action<string> aDelegate)
+        {
+            theDelegate = aDelegate;
+            theDispatcher = aDispatcher;
+            theTimer = new System.Timers.Timer();
+
+            theQueue = new Queue<CommercialEvent>();
+            theLastRelTime = TimeSpan.Zero;
+
+            theTimer.Elapsed += Run;
+            theTimer.Enabled = false;
+            theTimer.AutoReset = true;
+
+            theLock = new System.Object();
+
+
+        }
+
+        ~Scheduler()
+        {
+            theTimer.Enabled = false;
+        }
+
+        public void AddEvent(EventType aType, TimeSpan anOffset, string aCommercial)
+        {
+            CommercialEvent myEvent = new CommercialEvent();
+
+            myEvent.type = aType;
+            myEvent.offset = anOffset;
+            myEvent.commercial = aCommercial;
+
+            lock(theLock)
+            {
+                theQueue.Enqueue(myEvent);
+                theEnum = theQueue.GetEnumerator();
+                theEnum.MoveNext();
+            }
+        }
+
+        private void Run(Object source, ElapsedEventArgs ea)
+        {
+            lock(theLock)
+            {
+                theTimer.Enabled = false;
+
+                CommercialEvent myEvent = theEnum.Current;
+                theEnum.MoveNext();
+
+                if (myEvent.type == EventType.Show)
+                {
+                    theDispatcher.BeginInvoke(DispatcherPriority.Send, theDelegate, myEvent.commercial);
+                }
+                else
+                {
+                    theDispatcher.BeginInvoke(DispatcherPriority.Send, theDelegate, "");
+                }
+
+                theLastRelTime = myEvent.offset;
+                theLastTime = DateTimeOffset.Now;
+
+                Start();
+
+            }
+
+        }
+
+        public void Start()
+        {
+            lock(theLock)
+            {
+                try
+                {
+                    CommercialEvent myEvent = theEnum.Current;
+
+                    theTimer.Interval = (myEvent.offset - theLastRelTime).TotalMilliseconds;
+                    theLastTime = DateTimeOffset.Now;
+                    theTimer.Enabled = true;
+
+                }
+                catch(InvalidOperationException)
+                {
+                    // Do nothing
+                }
+ 
+            }
+
+        }
+
+        public void Stop()
+        {
+            lock (theLock)
+            {
+                theTimer.Enabled = false;
+                theLastRelTime = theLastRelTime + (DateTimeOffset.Now - theLastTime);
+            }
+        
+        }
+
+        public TimeSpan Position()
+        {
+            lock(theLock)
+            {
+                if (theTimer.Enabled)
+                {
+                    return theLastRelTime + (DateTimeOffset.Now - theLastTime);
+                }
+                else
+                {
+                    return theLastRelTime;
+                }
+            }
+        }
+
+        public TimeSpan Duration()
+        {
+            lock(theLock)
+            {
+                return theQueue.Last().offset;
+            }
+        }
+
+        public void SetPosition(TimeSpan aPosition)
+        {
+            lock(theLock)
+            {
+                bool myIsRunning = (theTimer.Enabled);
+                bool myIsBeforeFirstEvent = true;
+
+                Stop();
+
+                theEnum = theQueue.GetEnumerator();
+                theEnum.MoveNext();
+
+                // Points to event that is just before aPosition
+                Queue<CommercialEvent>.Enumerator myCurrentEventEnum = theQueue.GetEnumerator(); 
+
+                // Search next event to process and set theEnum to it
+                foreach (CommercialEvent e in theQueue)
+                {
+                    if (e.offset > aPosition)
+                    {
+                        break;
+                    }
+                    theEnum.MoveNext();
+                    myCurrentEventEnum.MoveNext();
+                    myIsBeforeFirstEvent = false;
+                }
+                theLastRelTime = aPosition;
+
+                if (!myIsBeforeFirstEvent)
+                {
+                    CommercialEvent myEvent = myCurrentEventEnum.Current;
+
+                    if (myEvent.type == EventType.Show && !myIsBeforeFirstEvent)
+                    {
+                        theDispatcher.BeginInvoke(DispatcherPriority.Send, theDelegate, myEvent.commercial);
+                    }
+                    else
+                    {
+                        theDispatcher.BeginInvoke(DispatcherPriority.Send, theDelegate, "");
+                    }
+
+                }
+                else
+                {
+                    theDispatcher.BeginInvoke(DispatcherPriority.Send, theDelegate, "");
+                }
+
+                if (myIsRunning)
+                {
+                    Start();
+                }
+
+            }
+
+
+        }
+    }
+}
---
+++ Commercials/UserControl1.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using vMixInterop;
+
+namespace Commercials
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl, vMixWPFUserControl
+    {
+        Scheduler theScheduler;
+        public UserControl1()
+        {
+            InitializeComponent();
+
+            theScheduler = new Scheduler(this.Dispatcher, Update);
+
+            theScheduler.AddEvent(Scheduler.EventType.Show, new TimeSpan(0, 0, 5), @"C:\Visual Studio Projects\Commercials\Commercials\bin\Debug\quoratio.PNG");
+            theScheduler.AddEvent(Scheduler.EventType.Hide, new TimeSpan(0, 0, 15), @"");
+            theScheduler.AddEvent(Scheduler.EventType.Show, new TimeSpan(0, 0, 20), @"C:\Visual Studio Projects\Commercials\Commercials\bin\Debug\kales.jpg");
+            theScheduler.AddEvent(Scheduler.EventType.Hide, new TimeSpan(0, 0, 30), @"");
+            theScheduler.AddEvent(Scheduler.EventType.Show, new TimeSpan(0, 0, 35), @"C:\Visual Studio Projects\Commercials\Commercials\bin\Debug\qfc.PNG");
+            theScheduler.AddEvent(Scheduler.EventType.Hide, new TimeSpan(0, 0, 45), @"");
+        }
+
+        private void Update(string aCommercial)
+        {
+            if (aCommercial != "")
+            {
+                BitmapImage mySource = new BitmapImage();
+
+                mySource.BeginInit();
+                mySource.UriSource = new Uri(aCommercial);
+                mySource.EndInit();
+
+                Image.Source = mySource;
+                Image.Visibility = System.Windows.Visibility.Visible;
+
+            }
+            else
+            {
+                Image.Visibility = System.Windows.Visibility.Hidden;
+            }
+ 
+        }
+
+        public void Close()
+        {
+            theScheduler.Stop();
+        }
+    
+
+        public TimeSpan GetDuration()
+        {
+            return theScheduler.Duration();
+        }
+
+        public TimeSpan GetPosition()
+        {
+            return theScheduler.Position();
+        }
+
+        public void Load(int width, int height)
+        {
+ 	        
+        }
+
+        public void Pause()
+        {
+            theScheduler.Stop();	        
+        }
+
+        public void Play()
+        {
+            theScheduler.Start();
+           
+        }
+
+        public void SetPosition(TimeSpan position)
+        {
+            theScheduler.SetPosition(position);
+        }
+
+        public void ShowProperties()
+        {
+ 	       
+        }
+}
+}
